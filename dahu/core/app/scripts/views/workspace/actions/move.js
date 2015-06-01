2bc9877cc2a313f81c003cdfbc38e69b41446120
Rémy Rey <remy.rey@ensimag.grenoble-inp.fr>
2015-06-01 10:48:54 +0200

    Introduce animation edition
    Display the different actions with an alternative menu when clicked.
    User can select the parameters via select lists and number inputs.
    
    Add :
    - Full templating with a central template 'action' and partials for each
    specific type of animations 'move', 'appear', 'disappear'.
    - inheritance between the views to control the templating and minimize
    the code redundancie
    - new set of events triggered by user actions in 'action' view. Associated
    callbacks for (re)rendering and model updates.
    - attributes in actions models to match the parameters used in preview
    
    Delete :
    - unused template appear and disappear.

--- dahu/core/app/scripts/views/workspace/actions/move.js
+++ dahu/core/app/scripts/views/workspace/actions/move.js
@@ -5,19 +5,49 @@
 define([
     'handlebars',
     'backbone.marionette',
+    // views
+    'views/workspace/actions/action',
     // templates
     'text!templates/views/workspace/actions/move.html'
 ], function(
     Handlebars,
     Marionette,
+    // views
+    ActionView,
     // templates
     moveTemplate
 ) {
+
+    var ExtraParamView = Marionette.ItemView.extend({
+
+        template: Handlebars.default.compile(moveTemplate),
+
+        initialize: function (model) {
+            this.model = model;
+        },
+
+        triggers: {
+                "change .trXChoice": "move:trx:change",
+                "change .trYChoice": "move:try:change"
+        }
+    });
     
     /**
      * Move action view
      */
-    return Marionette.ItemView.extend({
-        template: Handlebars.default.compile(moveTemplate)
+    return ActionView.extend({
+
+        onChildviewMoveTrxChange: function () {
+            this.model.set("trX", $("#trXChoice_" + this.model.id).val());
+        },
+
+        onChildviewMoveTryChange: function () {
+            this.model.set("trY", $("#trYChoice_" + this.model.id).val());
+        },
+
+        onRender: function () {
+            this.getRegion('extra').show(new ExtraParamView(this.model));
+        },
+
     });
 });
